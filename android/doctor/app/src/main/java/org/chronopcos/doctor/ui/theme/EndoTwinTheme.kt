3a2fd3523b0628dbe3d9b9dc6d265fbdbbbf8178
aviralsingh839 <aviralsingh839@gmail.com>
2026-09-21 22:14:30 +0530

    fix: remove duplicate Android theme import

--- android/doctor/app/src/main/java/org/chronopcos/doctor/ui/theme/EndoTwinTheme.kt
+++ android/doctor/app/src/main/java/org/chronopcos/doctor/ui/theme/EndoTwinTheme.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
